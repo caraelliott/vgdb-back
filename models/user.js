18c41486938bc105c65348861337dcff0cee8d9e
Cara <caraelliottwgsg@gmail.com>
2021-10-26 12:34:29 +0100

    added db fields to model
    amended model files

--- models/user.js
+++ models/user.js
@@ -1,2 +1,40 @@
 const { DataTypes } = require("sequelize");
 const connection = require("../connection");
+
+module.exports = connection.define("Game",
+{
+    id: {
+        type: DataType.INTEGER,
+        allowNull: false
+    },
+
+    name: {
+        type: DataType.STRING,
+        allowNull: false
+    },
+
+    year: {
+        type: DataType.INTEGER,
+        allowNull: false
+    },
+
+   console: {
+    type: DataType.STRING,
+    allowNull: false
+    },
+
+    genre: {
+        type: DataType.STRING,
+        allowNull: false
+    },
+
+    publisher: {
+        type: DataType.STRING,
+        allowNull: false
+    },
+},
+
+{
+    indexes: [{unique: true, fields: ["id"]}]
+
+});
