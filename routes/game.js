3abcf5e5dc437805c65b121b034ed8cb08256876
Cara <caraelliottwgsg@gmail.com>
2021-10-28 12:21:40 +0100

    attempt to implement edit/update
    added update to routes

--- routes/game.js
+++ routes/game.js
@@ -7,15 +7,15 @@ router.post("/", async (req, res) => res.status(201).json({ msg: "Add a Game", d
 router.delete("/", async (req, res) => res.status(200).json({ msg: "Delete all Games", data: await deleteAllGames() }));
 
 router.get("/:id", async (req, res) => res.status(200).json({ msg: "One Game", data: await oneGame(req.params.id, req.body) }));
+router.put("/:id", async (req,res, id) => res.status(201).json({ msg: "Edit One Game", data: await oneGame(req.params.id, req.body) }));
 
-// router.put("/:id", async (req, res, id) => {
-//     console.log("OI!")
-//     const game = allGames(req.params.id);
-//     if (!game) return res.status(404).json({});
-//     game.name = req.body.name;
-//     res.json(game)
-
-// });
+router.put("/:id", async (req, res, id) => {
+    console.log("OI!")
+    const game = allGames(req.params.id);
+    if (!game) return res.status(404).json({});
+    game.name = req.body.name;
+    res.json(game)
+});
 
 router.delete("/:id", async (req, res) => {
     deleteGame(req.params.id);
